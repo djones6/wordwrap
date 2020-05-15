8c472eecacaac8c036dcaa0208e5788679d5d407
David Jones <djones6@uk.ibm.com>
2020-05-15 14:43:24 +0100

    Trying to break into this wrap game

--- main.go
+++ main.go
@@ -22,7 +22,33 @@ func WordWrap(theString string, width int) string {
 		return theString
 	}
 
+	// Consider string[index:index+width+1] and look backward for whitespace
+	// if whitespace found split on it
+	// otherwise split at width
+	// index = whatever you split on
+	// carry on
+
+	currentPiece := theString[0 : width+1]
+	resultString := wrap(currentPiece, width)
+
 	firstBit := strings.TrimSpace(theString[0:width])
 	lastBit := strings.TrimSpace(theString[width:len(theString)])
 	return firstBit + "\n" + lastBit
+}
+
+// global resultString
+// func wrap (theString string, currentIndex int, width int) {
+//   if FINISHED return
+//   currentPiece := theString[currentIndex : width+1]
+//   resultString += Work out substring + '\n'
+//   return wrap(whitespaceindex:nextIndexAtSizeWidth)
+// }
+
+func wrap(currentPiece string, width int) string {
+	lastWhitespaceIndex := strings.LastIndex(currentPiece, " ")
+	if lastWhitespaceIndex > -1 {
+		return currentPiece[0:lastWhitespaceIndex] + "\n"
+	}
+
+	return currentPiece[0:width]
 }
