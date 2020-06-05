6ca3e44a1c0eeb80093cb6c9b9ab6dccc97b607d
David Jones <djones6@uk.ibm.com>
2020-06-05 14:36:40 +0100

    Victory

--- main.go
+++ main.go
@@ -1,54 +1,34 @@
 package wordwrap
 
 import (
-	"fmt"
-	"net/http"
 	"strings"
-	"time"
 )
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
-func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
-}
 
 // WordWrap wraps a string at the specified width
 func WordWrap(theString string, width int) string {
-	if len(theString) <= width {
-		return theString
-	}
-
-	// Consider string[index:index+width+1] and look backward for whitespace
-	// if whitespace found split on it
-	// otherwise split at width
-	// index = whatever you split on
-	// carry on
-
-	currentPiece := theString[0 : width+1]
-	resultString := wrap(currentPiece, width)
-
-	firstBit := strings.TrimSpace(theString[0:width])
-	lastBit := strings.TrimSpace(theString[width:len(theString)])
-	return firstBit + "\n" + lastBit
+	return wrap(theString, "", width)
 }
 
-// global resultString
-// func wrap (theString string, currentIndex int, width int) {
-//   if FINISHED return
-//   currentPiece := theString[currentIndex : width+1]
-//   resultString += Work out substring + '\n'
-//   return wrap(whitespaceindex:nextIndexAtSizeWidth)
-// }
-
-func wrap(currentPiece string, width int) string {
-	lastWhitespaceIndex := strings.LastIndex(currentPiece, " ")
-	if lastWhitespaceIndex > -1 {
-		return currentPiece[0:lastWhitespaceIndex] + "\n"
+func wrap(input string, output string, width int) string {
+	if len(input) == 0 {
+		return output
 	}
 
-	return currentPiece[0:width]
+	if len(input) <= width {
+		return output + input
+	}
+	currentPiece := input[0 : width+1]
+
+	lastWhitespaceIndex := strings.LastIndex(currentPiece, " ")
+	if lastWhitespaceIndex > -1 {
+		// freddy fox -> freddy\n
+		output = output + currentPiece[0:lastWhitespaceIndex] + "\n"
+		input = input[lastWhitespaceIndex+1:]
+	} else {
+		// bigbanana -> bigban\nana
+		output = output + currentPiece[0:width] + "\n"
+		input = input[width:]
+	}
+
+	return wrap(input, output, width)
 }
--- wordwrap_test.go
+++ wordwrap_test.go
@@ -46,3 +46,38 @@ func TestWrapOnWordBoundary(t *testing.T) {
 	expected := "freddy\nfox"
 	assert.Equal(t, expected, WordWrap(input, 6))
 }
+
+func TestWordSplitAcrossMultiLines(t *testing.T) {
+	input := "Supercalifragilisticexpialidocious"
+	expected := "Supercalif\nragilistic\nexpialidoc\nious"
+	assert.Equal(t, expected, WordWrap(input, 10))
+}
+
+func TestSuperCaliFragiWhatnot(t *testing.T) {
+	input := "It's supercalifragilisticexpialidocious Even though the sound of it Is something quite atrocious"
+	expected := "It's\nsupercalifragilistic\nexpialidocious Even\nthough the sound of\nit Is something\nquite atrocious"
+	assert.Equal(t, expected, WordWrap(input, 20))
+}
+
+func TestEssay(t *testing.T) {
+	input := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	expected := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\n" +
+		"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud\n" +
+		"exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute\n" +
+		"irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla\n" +
+		"pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\n" +
+		"deserunt mollit anim id est laborum."
+	assert.Equal(t, expected, WordWrap(input, 84))
+}
+
+func TestTrailingSpace(t *testing.T) {
+	input := "Vanilla   "
+	expected := "Vanilla   "
+	assert.Equal(t, expected, WordWrap(input, 15))
+}
+
+func TestLeadingSpace(t *testing.T) {
+	input := "   Vanilla"
+	expected := "   Vanilla"
+	assert.Equal(t, expected, WordWrap(input, 15))
+}
